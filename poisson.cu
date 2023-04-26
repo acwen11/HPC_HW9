b9ed07381ac6efac94eb7dd97399a50ec6107f0b
Allen Wen <acw6923@g.rit.edu>
2023-04-26 14:51:49 -0400

    fix large thread count, jacobi iteration still broken

--- poisson.cu
+++ poisson.cu
@@ -36,6 +36,8 @@ static double get_time(void)
 struct GFS
 {
   int n;
+	int nx;
+	int ny;
   gpu_fp dx2;
   gpu_fp dy2;
   gpu_fp idx2;
@@ -53,22 +55,26 @@ void jacobi(struct GFS gfs)
 {
   int ix = blockIdx.x * blockDim.x + threadIdx.x;
   int iy = blockIdx.y * blockDim.y + threadIdx.y;
+	// int ix = threadIdx.x;
+	// int iy = threadIdx.y;
   // int stride_x = blockDim.x * gridDim.x;
   // int stride_y = blockDim.y * gridDim.y;
-  const int n = gfs.n;
   const gpu_fp dx2 = gfs.dx2;
   const gpu_fp dy2 = gfs.dy2;
   const gpu_fp idx2 = gfs.idx2;
   const gpu_fp idy2 = gfs.idy2;
   const int nx = gfs.nx;
   const int ny = gfs.ny; 
-
  
   const int i = iy * nx + ix; 
 	const int ip1 = i + 1;
 	const int im1 = i - 1;
 	const int jp1 = i + nx; 
 	const int jm1 = i - nx;
+
+	// gfs.u_new[i] = gfs.src[i]; // Is this func doing anything at all?
+
+	// gfs.u_new[i] =  (0.5 * idx2 * idy2) * (dy2 * (gfs.u[ip1] + gfs.u[im1]) + dx2 * (gfs.u[jp1] + gfs.u[jm1]) - dx2 * dy2 * gfs.src[i]); // Issue with loop?
 
   if ((ix < nx - 1 && ix > 0) && (iy < ny - 1 && iy > 0)) // 2D Boundaries
   {
@@ -80,12 +86,14 @@ void jacobi(struct GFS gfs)
 __global__
  void copy (struct GFS gfs)
 {
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
-  //int stride = blockDim.x * gridDim.x;
-  const int n = gfs.n;
-  
-  const int i = index;
-  if (index < n-1 && index > 0)
+  int ix = blockIdx.x * blockDim.x + threadIdx.x;
+  int iy = blockIdx.y * blockDim.y + threadIdx.y;
+  const int nx = gfs.nx;
+  const int ny = gfs.ny; 
+ 
+  const int i = iy * nx + ix; 
+
+  if ((ix < nx - 1 && ix > 0) && (iy < ny - 1 && iy > 0)) // 2D Boundaries
   {
     gfs.u[i] = gfs.u_new[i];
   }
@@ -94,23 +102,28 @@ __global__
 __global__
  void error_check (struct GFS gfs)
 {
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
-  //int stride = blockDim.x * gridDim.x;
   const int n = gfs.n;
-  int ix = blockIdx.x * blockDim.x + threadIdx.x;//index_x
-  int iy = blockIdx.y * blockDim.y + threadIdx.y;//index_y
-  const int i = ix + n * iy
+  const int nx = gfs.nx;
+  const int ny = gfs.ny; 
+  int ix = blockIdx.x * blockDim.x + threadIdx.x;
+  int iy = blockIdx.y * blockDim.y + threadIdx.y;
+  const int i = ix + nx * iy;
+
 	const int ip1 = i + 1;
 	const int im1 = i - 1;
-	const int jp1 = i + n; 
-	const int jm1 = i - n;
+	const int jp1 = i + nx; 
+	const int jm1 = i - nx;
   const gpu_fp idx2 = gfs.idx2;
-  const gpu_fp idy2 = gfs.idy2
+  const gpu_fp idy2 = gfs.idy2;
 
-  if (i < n-1 && i > 0)
+
+	// gfs.error[i] = (gfs.u[ip1] + gfs.u[im1] - 2 * gfs.u[i]) * idx2 + (gfs.u[jp1] + gfs.u[jm1] - 2 * gfs.u[i]) * idy2 - gfs.src[i];
+	// gfs.error[i] *= gfs.error[i] /n;
+
+  if ((ix < nx - 1 && ix > 0) && (iy < ny - 1 && iy > 0)) // 2D Boundaries
   {
     gfs.error[i] = (gfs.u[ip1] + gfs.u[im1] - 2 * gfs.u[i]) * idx2 + (gfs.u[jp1] + gfs.u[jm1] - 2 * gfs.u[i]) * idy2 - gfs.src[i];
-    gfs.error[i] *= gfs.error[i] /n;
+    gfs.error[i] *= gfs.error[i] / n;
   }
 }
 
@@ -143,12 +156,14 @@ int main(int argc, char **argv)
   const gpu_fp dx = 1.0 / (N-1); //spacing between grid points in the x direction
   const gpu_fp dy = 1.0 / (N-1); //spacing in the y direction
   gfs_managed.n = N * N; //sets the grid points to N*N
+	gfs_managed.nx = N;
+	gfs_managed.ny = N;
   gfs_managed.dx2 = dx * dx;
   gfs_managed.idx2 = 1.0 / dx / dx; //sets the inverse of the squared spacing between grid points in the x-direction, which is also used in the finite difference approximations.
   gfs_managed.dy2 = dy * dy;
   gfs_managed.idy2 = 1.0 / dy / dy;
 
-  //initialize x and y arrays on the host TODO
+  //initialize x and y arrays on the host 
 	for (int j = 0; j < N; j++) {
 		for (int i = 0; i < N; i++) {
 			const gpu_fp x = i * dx;
@@ -161,8 +176,9 @@ int main(int argc, char **argv)
 		}
 	}
 
-  dim3 blockSize(256, 256);
-  dim3 numBlocks((N + blockSize.x - 1) / blockSize.x, (N + blockSize.y - 1) / blocksize.y);
+  dim3 blockSize(16, 16);
+  dim3 numBlocks((N + blockSize.x - 1) / blockSize.x, (N + blockSize.y - 1) / blockSize.y);
+	// int numBlocks = 1;
 
   const double t_start = get_time();
    
@@ -172,12 +188,25 @@ int main(int argc, char **argv)
   int its = 0;
   for (;;)
   {
-    for (int j = 0; j < 100; j++) //j was originally 1000, setting to 100 for testing
+    for (int j = 0; j < 1000; j++) 
     {
       jacobi <<<numBlocks, blockSize>>> (gfs_managed);
+			// Debug output ////////////////
+			for (int i = 0; i < N*N; i++)
+			{
+				printf("Unew set to be %20.16e\n", gfs_managed.u_new[i]); 
+				printf("using src of  %20.16e\n", gfs_managed.src[i]); 
+			}
+			///////////////////////////////
       copy <<<numBlocks, blockSize>>> (gfs_managed);
     }
     error_check <<<numBlocks, blockSize>>> (gfs_managed);
+		// // Debug output ////////////////
+		// for (int i = 0; i < N*N; i++)
+		// {
+		// 	printf("Error set to be %20.16e\n", gfs_managed.error[i]); 
+		// }
+		// ///////////////////////////////
     its ++ ;
     CUDA_CHECK(cudaDeviceSynchronize());
     const double error  = l2_error(gfs_managed);
@@ -200,8 +229,8 @@ int main(int argc, char **argv)
 		{
 			const double x = i * dx;
 			const double y = j * dy;
-			const double idx = j * N + i
-			printf("%20.16e %20.16e %20.16e\n",x, y, gfs_managed.u[idx]);
+			const int idx = j * N + i;
+			// printf("%20.16e %20.16e %20.16e\n", x, y, gfs_managed.u[idx]); TODO:reenable when ready
 		}
 	}
 
